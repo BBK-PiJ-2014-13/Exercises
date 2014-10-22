fa52173edbcd890be49d5d37c043724f3d4add02
Hadagan <MUt7aChe>
2014-10-22 13:56:28 +0100

    Finished exercise 1 from day 8

---
+++ 8.DayEightExercises/1.SupermarketQueue/Supermarket.java
@@ -0,0 +1,24 @@
+
+public class Supermarket implements PersonQueue{
+
+	void addPerson(Person p) {
+		
+	}
+	
+	void servePerson() {
+		
+	}
+
+	@Override
+	public void insert(Person person) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Person retrieve() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
