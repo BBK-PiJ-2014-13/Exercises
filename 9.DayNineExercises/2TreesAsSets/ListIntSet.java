5be1ae1023b607d49f7d28d03f9ac58e8cdca498
Hadagan <MUt7aChe>
2014-10-27 15:43:37 +0000

    Finished exercise 2 in day 9

---
+++ 9.DayNineExercises/2TreesAsSets/ListIntSet.java
@@ -0,0 +1,26 @@
+
+public class ListIntSet implements IntSet{
+
+	public ListIntSet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void add(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(int i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsVerbose(int i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ 9.DayNineExercises/2TreesAsSets/TreeIntSet.java
@@ -0,0 +1,26 @@
+
+public class TreeIntSet implements IntSet{
+
+	public TreeIntSet() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void add(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean contains(int i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsVerbose(int i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
