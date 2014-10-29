f73c166193efd9e0e34ab8e24a0dccd8cc2f4a76
Hadagan <MUt7aChe>
2014-10-29 14:42:50 +0000

    Finished exercise 5 in day 10

---
+++ 10.DayTen/5.MultipleInheritance/Guitar.java
@@ -0,0 +1,12 @@
+
+public class Guitar implements MusicalInstrument, WoodenObject{
+
+	@Override
+	public void burn() {
+	}
+
+	@Override
+	public void play() {
+	}
+
+}
---
+++ 10.DayTen/5.MultipleInheritance/WoodenObject.java
@@ -0,0 +1,5 @@
+
+public interface WoodenObject {
+	public void burn();
+
+}
