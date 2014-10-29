c05677c4a0823c0fb11a2bda02fc40d10f102f45
Hadagan <MUt7aChe>
2014-10-29 14:50:04 +0000

    Finished exercise 6 in day 10

---
+++ 10.DayTen/6.JavaMagic/StartingClass.java
@@ -0,0 +1,7 @@
+public class StartingClass {
+	public static void main(String[] args) {
+		Lecturer lecturer = new Lecturer("Mr. Johnson");
+		lecturer.doResearch("history");
+		lecturer.teach("math");
+	}
+}
