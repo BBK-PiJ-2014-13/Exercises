2ba8179790f47ce7fbb5b1ffafe17185ba629bff
Hadagan <MUt7aChe>
2014-11-03 15:39:05 +0000

    Finished exercise 5 in day 11, may add method bodies in future

---
+++ 11.DayEleven/5.GenericStack/Stack.java
@@ -0,0 +1,18 @@
+
+public class Stack<T extends Number> {
+	int a = 0;
+	int b = 5;
+	
+	public void push(T t) {
+		
+	}
+	public void pop() {
+		
+	}
+	public void peek() {
+		
+	}
+	public boolean isEmpty() {
+		return false;
+	}
+}
